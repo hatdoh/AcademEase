26a9b252923f10362f4e237c3f04ef268320f3b7
Xenia <yoyaycute@gmail.com>
2024-06-30 23:26:10 +0800

    UPDATED FIREBASE.

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,14 +1,11 @@
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, browserSessionPersistence, setPersistence } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDIu4AVozrEkyKEA_DYHxOZs0__OW2mObc",
   authDomain: "academease-c5a42.firebaseapp.com",
-
   databaseURL: "https://academease-c5a42-default-rtdb.asia-southeast1.firebasedatabase.app",
-
   projectId: "academease-c5a42",
   storageBucket: "academease-c5a42.appspot.com",
   messagingSenderId: "576932492833",
@@ -17,3 +14,13 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// Set persistence
+setPersistence(auth, browserSessionPersistence)
+  .catch((error) => {
+    console.error('Error setting persistence:', error);
+  });
+
+export { auth };
+export default app;
